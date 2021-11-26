e05046fc5183c003419978e22ad57ed843429dca
azengoose <72004245+azengoose@users.noreply.github.com>
2021-11-26 18:49:25 +1100

    Some additional changes to home page

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Glitchtest
-Created with CodeSandbox
+# MetaSite
+
+\*Has **absolutely zilch** affiliation with Meta
+
+MetaSite is a site that tries to be meta. Not too meta though – just a little bit.
+
+It discusses:
+
+- distinctions between coding, programming and (software) engineering,
+- about learning in general (and provides a few key pointers for learning on the web development side),
+- about projects & authenticity,
+- comparisons of self and community and fulfilment,
+- about discomfort & failure,
+- about existence of the web and existence as a human being
+- about social relationships and the network of history & geography, language & art
+
+MetaSite doesn't care about your opinions and loves you dearly. It was and is definitely not made by someone drifting upon the deafening, turbulent waves of existential malaise, on one end, unable to really come to terms with the sheer multiplicity of being a person in this day and age with interpretative values being thrown about in a world that, in some of those interpretations, comes across sometimes as absurd and meaningless.
+
+\*\*Until the web gets transferred into the Meta
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,14 +1,33 @@
 import * as React from "react";
 import { Link } from "wouter";
 
+const alternatives = [
+  "Why don't you click this button?",
+  "Wow, you just clicked on a link!",
+  "My, my, can you follow directions!",
+  "This is föcking INTERACTIVE???!!!"
+];
+function randomAlternative() {
+  return alternatives[Math.floor(Math.random() * alternatives.length)];
+}
+
 export default function Home() {
+  const [Alternatives, setAlternatives] = React.useState(alternatives[0]);
+  const handleAlternative = () => {
+    const newAlternative = randomAlternative();
+    setAlternatives(newAlternative);
+  };
+
   return (
     <>
       <h1>&lt;MetaSite/&gt;</h1>
       <p className="sub-text">
-        <strong>Why</strong> and <strong>How</strong> does this site exist &{" "}
-        <br /> who the hell cares anyway?
+        <strong>Why</strong> and <strong>How</strong> do I exist & who or rather{" "}
+        what the hell cares anyway?
       </p>
+      <button className="btn--cool" onClick={handleAlternative}>
+        {Alternatives}
+      </button>
       <div className="instructions">
         <h2>Change and Variation</h2>
         <p>
@@ -18,13 +37,10 @@ export default function Home() {
           page, and check out README.md in the editor for additional detail plus
           next steps you can take!
         </p>
-        <h2>Change and Variation</h2>
+        <h2>Contents</h2>
         <p>
-          This is a <strong>React</strong> project. That just means that these
-          web pages were created using something called React. This is a
-          React-flavoured website, if you will. <Link href="/about">About</Link>{" "}
-          page, and check out README.md in the editor for additional detail plus
-          next steps you can take!
+          - Computer Stuff - Learning - Projects - Fulfilment - Discomfort &
+          Failure - Existence - Social Worlds
         </p>
       </div>
     </>
