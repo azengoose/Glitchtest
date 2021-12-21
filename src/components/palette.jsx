970bf660e5b391a485405b9bee5110f8b09c146f
azengoose <72004245+azengoose@users.noreply.github.com>
2021-12-21 18:57:14 +1100

    Still need to fix local storage

--- src/components/palette.jsx
+++ src/components/palette.jsx
@@ -1,8 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/palettestyles.css";
 
 export default function Palette() {
-  const [palette, setPalette] = useState("light-theme");
+  const [palette, setPalette] = useState(
+    "light-theme",
+    localStorage.getItem("theme") === "true"
+  );
+
+  //the useEffect hook to compare previous and current state
+  useEffect(() => {
+    localStorage.setItem("theme", palette);
+  }, [palette]);
 
   function toggleTheme() {
     setPalette("dark-theme");
